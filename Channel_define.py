b9e1733541ac1492c988897780d13cf68184490f
ni-soa <77195064+ni-soa@users.noreply.github.com>
2021-02-25 17:04:20 +0900

    Delete Channel_define.py

--- Channel_define.py
+++
@@ -1,8 +0,0 @@
-import discord
-client = discord.Client()
-
-@client.event
-async def on_message(message):
-  if message.content == "Member status":
-    Status_channel = message.channel
-    print(Status_channel)
